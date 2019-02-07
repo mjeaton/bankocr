4c7530bc99088ed4f2733ed895827fd9b52e8f74
Michael Eaton <mjeaton@gmail.com>
2019-02-07 09:48:10 -0500

    comments to guide implementation

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,18 @@ namespace src
         static void Main(string[] args)
         {
             Console.WriteLine("Bank OCR Kata");
+
+            // Story 1
+            // read file and parse data into numbers
+
+            // Story 2
+            // validation of numbers
+
+            // Story 3
+            // formatted output file
+
+            // Story 4
+            // deal with ambiguous numbers
         }
     }
 }
