590ccf0b6047878c99ba7996e515e6db122eea4c
Michael Eaton <mjeaton@gmail.com>
2019-02-07 13:56:43 -0500

    First pass at validation

--- src/NumberValidator.cs
+++ src/NumberValidator.cs
@@ -1,10 +1,30 @@
+using System.Linq;
+
 namespace BankOCR
 {
     public class NumberValidator
     {
         public bool IsValid(string number)
         {
-            return true;
+            if(number.Length != 9)
+                return false;
+
+            var chars = number.ToCharArray();
+
+            var d1 = char.GetNumericValue(chars[8]);
+            var d2 = char.GetNumericValue(chars[7]);
+            var d3 = char.GetNumericValue(chars[6]);
+            var d4 = char.GetNumericValue(chars[5]);
+            var d5 = char.GetNumericValue(chars[4]);
+            var d6 = char.GetNumericValue(chars[3]);
+            var d7 = char.GetNumericValue(chars[2]);
+            var d8 = char.GetNumericValue(chars[1]);
+            var d9 = char.GetNumericValue(chars[0]);
+
+            double result = d1 + (2*d2) + (3*d3) + (4*d4) + (5*d5) + (6*d6) + (7*d7) + (8*d8) + (9*d9);
+            var checksum = result % 11;
+
+            return checksum == 0;
         }
     }
 }
--- tests/NumberValidatorTests.cs
+++ tests/NumberValidatorTests.cs
@@ -5,11 +5,20 @@ namespace BankOCR.Tests
     public class NumberValidatorTests
     {
         [Fact]
+        public void Short_Number_Should_Return_IsValid_Falswe()
+        {
+            var validator = new NumberValidator();
+
+            Assert.False(validator.IsValid("3458"));
+        }
+
+        [Fact]
         public void Valid_Number_Should_Return_IsValid_True()
         {
             var validator = new NumberValidator();
 
-            Assert.True(validator.IsValid("123456789"));
+            var result = validator.IsValid("345882865");
+            Assert.True(result);
         }
     }
 }
