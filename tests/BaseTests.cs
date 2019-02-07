70771c47588628f115b557ffdf20c432fb947ac5
Michael Eaton <mjeaton@gmail.com>
2019-02-07 10:49:35 -0500

    Fixed initial test since an array index was incorrect. Added 2nd test.

--- tests/BaseTests.cs
+++ tests/BaseTests.cs
@@ -17,15 +17,15 @@ namespace BankOCR.Tests
             };
 
             // basically, a bunch of substring operations
-            var digitOne = $"{data[0].Substring(0,3)}\r\n{data[1].Substring(0,3)}\r\n{data[0].Substring(0,3)}";
-            var digitTwo = $"{data[0].Substring(3,3)}\r\n{data[1].Substring(3,3)}\r\n{data[0].Substring(3,3)}";
-            var digitThree = $"{data[0].Substring(6,3)}\r\n{data[1].Substring(6,3)}\r\n{data[0].Substring(6,3)}";
-            var digitFour = $"{data[0].Substring(9,3)}\r\n{data[1].Substring(9,3)}\r\n{data[0].Substring(9,3)}";
-            var digitFive = $"{data[0].Substring(12,3)}\r\n{data[1].Substring(12,3)}\r\n{data[0].Substring(12,3)}";
-            var digitSix = $"{data[0].Substring(15,3)}\r\n{data[1].Substring(15,3)}\r\n{data[0].Substring(15,3)}";
-            var digitSeven = $"{data[0].Substring(18,3)}\r\n{data[1].Substring(18,3)}\r\n{data[0].Substring(18,3)}";
-            var digitEight = $"{data[0].Substring(21,3)}\r\n{data[1].Substring(21,3)}\r\n{data[0].Substring(21,3)}";
-            var digitNine = $"{data[0].Substring(24,3)}\r\n{data[1].Substring(24,3)}\r\n{data[0].Substring(24,3)}";
+            var digitOne = $"{data[0].Substring(0,3)}\r\n{data[1].Substring(0,3)}\r\n{data[2].Substring(0,3)}";
+            var digitTwo = $"{data[0].Substring(3,3)}\r\n{data[1].Substring(3,3)}\r\n{data[2].Substring(3,3)}";
+            var digitThree = $"{data[0].Substring(6,3)}\r\n{data[1].Substring(6,3)}\r\n{data[2].Substring(6,3)}";
+            var digitFour = $"{data[0].Substring(9,3)}\r\n{data[1].Substring(9,3)}\r\n{data[2].Substring(9,3)}";
+            var digitFive = $"{data[0].Substring(12,3)}\r\n{data[1].Substring(12,3)}\r\n{data[2].Substring(12,3)}";
+            var digitSix = $"{data[0].Substring(15,3)}\r\n{data[1].Substring(15,3)}\r\n{data[2].Substring(15,3)}";
+            var digitSeven = $"{data[0].Substring(18,3)}\r\n{data[1].Substring(18,3)}\r\n{data[2].Substring(18,3)}";
+            var digitEight = $"{data[0].Substring(21,3)}\r\n{data[1].Substring(21,3)}\r\n{data[2].Substring(21,3)}";
+            var digitNine = $"{data[0].Substring(24,3)}\r\n{data[1].Substring(24,3)}\r\n{data[2].Substring(24,3)}";
 
             var segments = new List<string>()
             {
@@ -40,6 +40,26 @@ namespace BankOCR.Tests
                 digitNine
             };
             Assert.True(segments.Count == 9);
+        }
+
+        [Fact]
+        public void Valid_Segment_Should_Be_A_Number()
+        {
+            var data = new string[]
+            {
+            "    _  _     _  _  _  _  _ ",
+            "  | _| _||_||_ |_   ||_||_|",
+            "  ||_  _|  | _||_|  ||_| _|"
+            };
+
+            // basically, a bunch of substring operations
+            var digitOne = $"{data[0].Substring(0,3)}\r\n{data[1].Substring(0,3)}\r\n{data[2].Substring(0,3)}";
+
+            var one = "   \r\n" +
+                      "  |\r\n" +
+                      "  |";
+
+            Assert.True(digitOne == one);
         }
     }
 }
