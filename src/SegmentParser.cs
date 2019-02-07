a0a9c8f6b8abdb25a08a83f68461fef5786398a6
Michael Eaton <mjeaton@gmail.com>
2019-02-07 12:14:46 -0500

    Segment parsing

---
+++ src/SegmentParser.cs
@@ -0,0 +1,71 @@
+namespace BankOCR
+{
+    public class SegmentParser
+    {
+        // candidate for refactoring
+        public string GetNumberForSegment(string segment)
+        {
+            if(segment == " _ \r\n" +
+                          "| |\r\n" +
+                          " _\r\n"
+            )
+                return "0";
+
+            if(segment == "   \r\n" +
+                          "  |\r\n" +
+                          "  |"
+            )
+                return "1";
+
+            if(segment == " _ \r\n" +
+                          " _|\r\n" +
+                          "|_ "
+            )
+                return "2";
+
+            if(segment == " _ \r\n" +
+                          " _|\r\n" +
+                          " _|"
+            )
+                return "3";
+
+            if(segment == "   \r\n" +
+                          "|_|\r\n" +
+                          "  |"
+            )
+                return "4";
+
+            if(segment == " _ \r\n" +
+                          "|_ \r\n" +
+                          " _|"
+            )
+                return "5";
+
+            if(segment == " _ \r\n" +
+                          "|_ \r\n" +
+                          "|_|"
+            )
+                return "6";
+
+            if(segment == " _ \r\n" +
+                          "  |\r\n" +
+                          "  |"
+            )
+                return "7";
+
+            if(segment == " _ \r\n" +
+                          "|_|\r\n" +
+                          "|_|"
+            )
+                return "8";
+
+            if(segment == " _ \r\n" +
+                          "|_|\r\n" +
+                          " _|"
+            )
+                return "9";
+            
+            return "";
+        }
+    }
+}
---
+++ tests/SegmentParserTests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace BankOCR.Tests
+{
+    public class SegmentParserTests
+    {
+        [Fact]
+        public void Valid_One_Segment_Should_Return_1()
+        {
+            var parser = new SegmentParser();
+            var segment = "   \r\n" +
+                          "  |\r\n" +
+                          "  |";
+
+            var result = parser.GetNumberForSegment(segment);
+            Assert.True(result == "1");
+        }
+
+        [Fact]
+        public void Valid_Two_Segment_Should_Return_2()
+        {
+            var parser = new SegmentParser();
+            var segment = " _ \r\n" +
+                          " _|\r\n" +
+                          "|_ ";
+
+            var result = parser.GetNumberForSegment(segment);
+
+            System.Console.WriteLine(result);
+            Assert.True(result == "2");
+        }
+
+    }
+}
