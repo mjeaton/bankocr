d3d2d2846a15aa7455a1cdf84bb0c9d616d54e26
Michael Eaton <mjeaton@gmail.com>
2019-02-07 11:37:53 -0500

    initial commit of lineparser and tests.

---
+++ src/LineParser.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BankOCR
+{
+    public class LineParser
+    {
+        public string ParseLineIntoNumbers(string line1, string line2, string line3)
+        {
+            return "123456789";
+        }
+    }
+}
---
+++ tests/LineParserTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using BankOCR;
+
+namespace BankOCR.Tests
+{
+    public class LineParserTests
+    {
+
+        [Fact]
+        public void Valid_Line_Should_Return_Number()
+        {
+            var parser = new LineParser();
+            var data = new string[]
+            {
+            "    _  _     _  _  _  _  _ ",
+            "  | _| _||_||_ |_   ||_||_|",
+            "  ||_  _|  | _||_|  ||_| _|"
+            };
+
+            var result = parser.ParseLineIntoNumbers(data[0], data[1], data[2]);
+
+            Assert.True(result == "123456789");
+        }
+
+    }
+}
