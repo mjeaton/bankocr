52a9be38f1e1a98f66769a0864c272056b7d1fe1
Michael Eaton <mjeaton@gmail.com>
2019-02-07 16:06:18 -0500

    Start of a theory

--- tests/SegmentParserTests.cs
+++ tests/SegmentParserTests.cs
@@ -27,5 +27,15 @@ namespace BankOCR.Tests
             var result = parser.GetNumberForSegment(segment);
             Assert.True(result == "2");
         }
+
+        [Theory]
+        [InlineData("   \r\n  |\r\n  |", "1")]
+        public void Valid_Segment_Should_Return_Number(string segment, string expectedResult)
+        {
+            var parser = new SegmentParser();
+            var result =parser.GetNumberForSegment(segment);
+            System.Console.WriteLine(result);
+            Assert.True(result == expectedResult);
+        }
     }
 }
